7e19c55f47c8f7824508b26e50ac17a7d2674759
Victoria  Topham <topham.victoria@gmail.com>
2020-03-25 12:19:52 -0700

    got the form elements in and some styling

--- fun-with-users/src/Components/Form.js
+++ fun-with-users/src/Components/Form.js
@@ -1,11 +1,56 @@
 import React from "react"
+import styled from "styled-components"
+
+const FormStyle = styled.form`
+    * {
+        margin: 1% 0;
+    }
+
+    button {
+        height: 40px;
+        width: 90px;
+        border-radius: 10px;
+        background: #09BF4A;
+        color: white;
+        font-size: 16px;
+        border: 1px solid #09BF4A;
+        box-shadow: 2px 2px 5px gray;
+    }
+
+    .disabled {
+        background: lightgray;
+        border: none;
+        box-shadow: none;
+        
+    }
+
+    .checkbox-label {
+        margin-left: 10px;
+    }
 
 
+`
 function Form(props) {
 
 
     return (
         <>
+        
+        <FormStyle>
+            <label htmlFor = "name">Name </label>
+            <input type = "text" id = "name" name = "name"/>
+            <br/>
+            <label htmlFor = "email">Email </label>
+            <input type = "text" id = "email" name = "email"/>
+            <br/>
+            <label htmlFor = "password">Password </label>
+            <input type = "password" id = "password" name = "password"/>
+            <br/>
+            <input type = "checkbox" id = "terms" name = "terms"/>
+            <label hrmlFor = "terms" className = "checkbox-label">Do you agree to the terms of service?</label>
+            <br/>
+            <button>Submit</button>
+        </FormStyle>
         </>
     )
 
