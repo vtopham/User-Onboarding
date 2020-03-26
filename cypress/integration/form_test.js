b495db9b38bd356b6bbd16af8d2e53122dd08b51
Victoria  Topham <topham.victoria@gmail.com>
2020-03-26 12:17:08 -0700

    test checks inputted name, email, password, and checks the box successfully

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -6,7 +6,20 @@ describe("this is a block of tests", function () {
 
     it("checking step 2 instructions", function () {
 
-        cy.get('input[name = "name"]')
-        .type("Victoria");
+        cy.get('input[name = "name"]') //type and check name
+        .type("Victoria")
+        .should("have.value", "Victoria");
+
+        cy.get('input[name = "email"]') //type and check email
+        .type("victoria@gmail.com")
+        .should("have.value", "victoria@gmail.com");
+
+        cy.get('input[name = "password"]') //type and check password
+        .type("hellothere")
+        .should("have.value", "hellothere");
+
+        cy.get('input[name = "terms"]') //type and check the checkbox
+        .check()
+        .should("be.checked", "true");
     })
 })
