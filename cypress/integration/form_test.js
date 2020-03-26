1ac1c8e5be5463b2fa3232e47d9c4bbad75270d6
Victoria  Topham <topham.victoria@gmail.com>
2020-03-26 16:36:38 -0700

    added more blank functionality per Juan's tip

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -6,9 +6,15 @@ describe("this is a block of tests", function () {
 
     it("checking step 2 instructions", function () {
 
+        cy.get("button")//make sure blank name is invalid
+        .should("have.disabled", "true");
+
         cy.get('input[name = "name"]') //type and check name
         .type("Victoria")
         .should("have.value", "Victoria");
+
+        cy.get("button")//make sure blank email is invalid
+        .should("have.disabled", "true");
 
         cy.get('input[name = "email"]') //type and check email that FAILS
         .type("victoria");
@@ -21,15 +27,22 @@ describe("this is a block of tests", function () {
         .type("@gmail.com")
         .should("have.value", "victoria@gmail.com");
 
+        cy.get("button")//make sure blank password is invalid
+        .should("have.disabled", "true");
+
         cy.get('input[name = "password"]') //type and check password
         .type("hellothere")
         .should("have.value", "hellothere");
+
+        cy.get("button")//make sure unchcekd checkbox is invalid
+        .should("have.disabled", "true");
 
         cy.get('input[name = "terms"]') //type and check the checkbox
         .check()
         .should("be.checked", "true");
 
-        cy.get("button") //submits data
+
+        cy.get("button") //submits data, checks that button should be valid now
         .should("not.be.disabled")
         .click();
         
